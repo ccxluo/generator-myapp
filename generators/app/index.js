746f7aae92e87457d47e0181d5502b9ffb42040a
XLuoChen <cxluo@thoughtworks.com>
2019-08-13 19:15:24 +0800

    init generator

---
+++ generators/app/index.js
@@ -0,0 +1,17 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  // The name `constructor` is important here
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    // Next, add your custom code
+    this.option('babel'); // This method adds support for a `--babel` flag
+  }
+
+  // add your own methods
+  method1() {
+    console.log('I am a custom method');
+  }
+};
